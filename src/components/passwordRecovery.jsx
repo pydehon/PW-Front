8fb62c1d409144b21e17c780cab96b1c02dc306d
Pierre-Yves <pydehon@gmail.com>
2021-11-16 19:44:21 +0000

    fix react-router

---
+++ src/components/passwordRecovery.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/passwordRecovery.scss';
+
+class PasswordRecvoery extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="prroot">
+               
+            </div>
+        )
+    }
+}
+
+export default PasswordRecvoery;
